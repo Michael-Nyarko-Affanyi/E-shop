18c4a9e61c5f8ed88b588c2b60c601702229fc74
Michael-Nyarko-Affanyi <cwajodepay@gmail.com>
2023-04-26 15:33:12 +0000

    feature: project initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className=' text-red-500'>
+        <h1>hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
